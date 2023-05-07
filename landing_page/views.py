d16451cfda2735382127aa4a94fe0c271e36183c
shafaleyda <shafaleyda@gmail.com>
2023-05-07 13:54:34 +0700

    Fixed routing for roles authorization

--- landing_page/views.py
+++ landing_page/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-def show_landing_page(request):
-    return render(request, "landing_page.html")
+def show_landing_page_manajer(request):
+    return render(request, "landing_page_manajer.html")
+
+def show_landing_page_penonton(request):
+    return render(request, "landing_page_penonton.html")
+
+def show_landing_page_panitia(request):
+    return render(request, "landing_page_panitia.html")
