6d5d90e1ef039eac211bdf139741780d4893c4dd
shafaleyda <shafaleyda@gmail.com>
2023-05-07 15:38:18 +0700

    Fixed settings.py

--- membuat_pertandingan/apps.py
+++ membuat_pertandingan/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MembuatPertandinganConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'membuat_pertandingan'
+# class MembuatPertandinganConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'membuat_pertandingan'
