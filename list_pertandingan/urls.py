a2fe727e4cca3a80a748408f3ef93f87c7d33062
shafaleyda <shafaleyda@gmail.com>
2023-05-25 10:02:45 +0700

    Add session for id manajer, panitia, penonton

--- list_pertandingan/urls.py
+++ list_pertandingan/urls.py
@@ -4,11 +4,6 @@ from list_pertandingan.views import *
 app_name = 'list_pertandingan'
 
 urlpatterns = [
-<<<<<<< HEAD
     path('manajer/', list_pertandingan_manager, name='list_pertandingan_manager'),
     path('penonton/', list_pertandingan_penonton, name='list_pertandingan_penonton'),
-=======
-    path('penonton/', list_pertandingan_penonton, name='list_pertandingan_penonton'),
-    path('manajer/', list_pertandingan_manager, name='list_pertandingan_manajer'),
->>>>>>> 3d8f74a49d196fa18e718fda8403587dd6921851
 ]
